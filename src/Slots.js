8558dcf969e51be78288044558655e07cf5c1d14
Dario Mercuri <dario.mercuri31@gmail.com>
2022-02-21 23:17:53 -0500

    basic functionality complete

--- src/Slots.js
+++ src/Slots.js
@@ -1,4 +1,4 @@
-import React,{useRef} from 'react';
+import React,{useRef, useState} from 'react';
 import styled, {ThemeProvider } from 'styled-components';
 import GlobalStyles from "./GlobalStyle";
 
@@ -6,16 +6,82 @@ import { Panel,TextField,Button } from 'react95';
 import original from "react95/dist/themes/original";
 
 const fruits = ["🍒", "🍉", "🍊", "🍓", "🍇", "🥝"]
+let ongoingGame = false;
 
 export const Slots = () => {
     let slotRef = [useRef(null), useRef(null), useRef(null)];
+    const [rolling,setRolling] = useState(false)
+    const [result,setResult] = useState("-")
+
+    function roll(){
+        setResult("-")
+        ongoingGame = true;
+        setRolling(true)
+
+        let slot1,slot2,slot3 = null;
+        slotRef.forEach((slot, i) => {
+            const selected = triggerSlotRotation(slot.current);
+            if(i+1 == 1){
+                slot1 = selected;
+            }
+            else if(i+1 == 2){
+                slot2 = selected;
+            }
+            else{
+                slot3 = selected;
+            } 
+        });
+
+        setTimeout(() => {
+            setRolling(false)
+            checkOutcome([slot1,slot2,slot3])
+        }, 700)
+        
+    }
+
+    const triggerSlotRotation = ref => {
+        function setTop(top) {
+            ref.style.top = `${top}px`;
+        }
+        let options = ref.children;
+        let randomOption = Math.floor(
+          Math.random() * fruits.length
+        );
+        let choosenOption = options[randomOption];
+        setTop(-choosenOption.offsetTop + 2);
+        return fruits[randomOption];
+    };
+
+    //CHECK OUTCOME
+    function checkOutcome(slotsArray){
+        console.log("CHECKING OUTCOME")
+            // let slotsArray = Object.keys(slots).map((key) => [slots[key]]);
+            let counts = {};
+            for (const fruit of slotsArray) {
+                counts[fruit] = counts[fruit] ? counts[fruit] + 1 : 1;
+            }
+            slotsArray.forEach(fruit => {
+                if(ongoingGame){
+                    console.log(counts[fruit]);
+                    if(counts[fruit] === 2){
+                        setResult("WIN DOUBLE!")
+                        ongoingGame = false;
+                    }else if(counts[fruit] === 3){
+                        setResult("WIN TRIPLE")
+                        ongoingGame = false;
+                    }
+                }
+            });
+    }
+
     return (
         <ThemeProvider theme={original}>
         <Container>
             <SlotContainer>
                 <h1>Slots</h1>
-                <p>-</p>
+                <p>{result}</p>
                 <FruitContainers>
+                    <Slot variant='well'>
                         <SlotSection>
                             <FruitContainer ref={slotRef[0]}>
                             {fruits.map((fruit,i)=>(
@@ -25,6 +91,8 @@ export const Slots = () => {
                             ))}
                             </FruitContainer>
                         </SlotSection>
+                    </Slot>
+                    <Slot variant='well'>
                         <SlotSection>
                                 <FruitContainer ref={slotRef[1]}>
                                 {fruits.map((fruit,i)=>(
@@ -34,6 +102,8 @@ export const Slots = () => {
                                 ))}
                                 </FruitContainer>
                         </SlotSection>
+                    </Slot>
+                    <Slot variant='well'>
                         <SlotSection>
                             <FruitContainer ref={slotRef[2]}>
                             {fruits.map((fruit,i)=>(
@@ -43,11 +113,9 @@ export const Slots = () => {
                             ))}
                             </FruitContainer>
                         </SlotSection>
+                    </Slot>
                 </FruitContainers>
-                <div style={{display: 'flex'}}>
-                    <TextField placeholder='Bet'/>
-                    <Button>Roll</Button>
-                </div>
+                <Button disabled={rolling} onClick={() => roll()}>Roll</Button>
             </SlotContainer>
         </Container>
     </ThemeProvider>
@@ -68,8 +136,8 @@ const Container = styled.div`
     margin: auto;
 `
 const SlotContainer = styled(Panel)`
-  width: 100%;
-  height: 100%;
+    min-width: 400px;
+  min-height: 400px;
   text-align: center;
   display: flex;
   flex-direction: column;
@@ -82,17 +150,29 @@ const FruitContainers = styled.div`
   padding: 20px;
 `
 const SlotSection = styled.section`
-    width: 70px;
-    height: 70px;
+    position: relative;
+    width: 100px;
+    height: 100px;
     overflow: hidden;
-    line-height: 50px;
-    margin: 3px;
+    color: white;
+    font-family: sans-serif;
+    text-align: center;
+    vertical-align: middle;
+    top: 2px;
+    left: 2px;
+    font-size: 40px;
+    line-height: 90px;
 `
-const FruitContainer = styled(Panel)`
-    height: 100%;
+const FruitContainer = styled.div`
+    position: relative;
+    top: 2px;
+    height: 100px;
     width: 100%;
-    font-size: xx-large;
     transition: top ease-in-out 0.7s;
     text-align: center;
-    padding: 10px;
+`
+const Slot = styled(Panel)`
+    min-height: 100px;
+    min-width: 100px;
+    margin: 0px 5px;
 `
